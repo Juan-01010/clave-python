be504c3c9d90cefb8bf5395c4ec18b2a70717148
Juan-01010 <juanalbertodelaguilachavez11@gmail.com>
2024-08-06 18:50:17 -0500

    Create work.py
    creando archivo para una clave de seguridad

---
+++ work.py
@@ -0,0 +1,12 @@
+import random
+
+def create_password(pass_length):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    print(password)
+
+create_password(number)
